07a21705c350d387324b81089cabe0f08d743a0c
Richard Zheng <rjzhengweb@gmail.com>
2024-11-09 23:47:28 -0800

    send release changelog to slack and handle expo versions

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,49 @@
-import { getInput } from "@actions/core";
 import { LinearClient } from "@linear/sdk";
-import { getVersion, getRepoName, createLabel, getDoneStatus, updateIssues } from "./utils";
+import {
+  getVersion,
+  getRepoName,
+  getDoneStatus,
+  updateIssues,
+  sendToSlack,
+  compileChangelog,
+  getInput,
+  createVersionLabel,
+} from "./utils";
 import * as core from "@actions/core";
+import { WebClient } from "@slack/web-api";
+
+import * as dotenv from "dotenv";
+
+// Used for local development
+dotenv.config({ path: ".env.local" });
 
 export async function run() {
   try {
+    // Get inputs from action
     const apiKey = getInput("linearApiKey");
-    const client = new LinearClient({ apiKey });
+    const slackToken = getInput("slackToken");
+    const slackChannel = getInput("slackChannel");
 
+    // Initialize clients
+    const client = new LinearClient({ apiKey });
+    const slackClient = new WebClient(slackToken);
+
+    // Get version and repo name
     const version = getVersion();
     const repoName = getRepoName();
 
-    const versionLabel = await createLabel(client, version, repoName);
+    const versionLabel = await createVersionLabel({client, version, repoName});
     const doneStatus = await getDoneStatus(client);
 
-    await updateIssues(client, {
+    const issues = await updateIssues(client, {
       versionLabel,
       repoName,
       stateId: doneStatus.id,
     });
+
+    const changelog = await compileChangelog({ issues, repoName, version });
+    await sendToSlack(slackClient, slackChannel, changelog);
+
     core.info("Done.");
   } catch (error) {
     if (error instanceof Error) {
--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,21 @@
 import * as core from "@actions/core";
-import { IssueLabel, LinearClient, WorkflowState } from "@linear/sdk";
+import { Issue, IssueLabel, LinearClient, WorkflowState } from "@linear/sdk";
 import * as fs from "fs";
-import * as dotenv from "dotenv";
+import { WebClient } from "@slack/web-api";
 
+import * as dotenv from "dotenv";
 dotenv.config({ path: ".env.local" });
 
 export function getVersion(): string {
+  try {
+    const appJson = JSON.parse(fs.readFileSync("app.json", "utf8"));
+    if (appJson) {
+      return `${appJson.expo.version}-${appJson.expo.extra.ota.version}`;
+    }
+  } catch {
+    // no-op
+  }
+
   const packageJson = JSON.parse(fs.readFileSync("package.json", "utf8"));
   return packageJson.version;
 }
@@ -18,11 +28,20 @@ export function getInput(name: string): string {
   return process.env[name] ?? core.getInput(name);
 }
 
-export async function createLabel(
-  client: LinearClient,
-  version: string,
-  repoName: string
-): Promise<IssueLabel> {
+export function reportErrorAndExit(message: string): never {
+  core.setFailed(message);
+  throw new Error(message);
+}
+
+export async function createVersionLabel({
+  client,
+  version,
+  repoName,
+}: {
+  client: LinearClient;
+  version: string;
+  repoName: string;
+}): Promise<IssueLabel> {
   core.info(`Creating a new label for version: ${version}...`);
   const parentLabelNodes = await client.issueLabels({
     filter: {
@@ -76,13 +95,14 @@ export async function createLabel(
     core.info(`Found label with name: ${version}`);
   }
   if (!versionLabel) {
-    core.setFailed(`Failed to create or find label ${version} in Linear.`);
-    throw new Error(`Failed to create or find label ${version} in Linear.`);
+    reportErrorAndExit(`Failed to create or find label ${version} in Linear.`);
   }
   return versionLabel;
 }
 
-export async function getDoneStatus(client: LinearClient): Promise<WorkflowState> {
+export async function getDoneStatus(
+  client: LinearClient
+): Promise<WorkflowState> {
   const statusNodes = await client.workflowStates({
     filter: {
       name: {
@@ -120,8 +140,11 @@ export async function updateIssues(
       },
     },
   });
-  core.info(`Found ${issuesToUpdate.nodes.length} issues to update.`);
+  if (issuesToUpdate.nodes.length === 0) {
+    reportErrorAndExit("No issues found to update.");
+  }
 
+  core.info(`Found ${issuesToUpdate.nodes.length} issues to update.`);
   for (const issue of issuesToUpdate.nodes) {
     core.info(`Updating issue ${issue.identifier}...`);
     try {
@@ -136,4 +159,52 @@ export async function updateIssues(
       core.warning(`Failed to update issue ${issue.identifier}: ${error}`);
     }
   }
+
+  return issuesToUpdate.nodes;
+}
+
+export async function compileChangelog({
+  issues,
+  repoName,
+  version,
+}: {
+  issues: Issue[];
+  repoName: string;
+  version: string;
+}): Promise<string> {
+  const changelog = [
+    `*Release Notes: \`${repoName}-${version}\`* has been successfully released! :rocket:`,
+    `*Release Date:* ${new Date().toLocaleDateString()}`,
+    `*Total Issues:* ${issues.length}\n`,
+    "Here's a summary of the completed issues:",
+  ];
+
+  for (const issue of issues) {
+    try {
+      const assignee = await issue.assignee;
+      changelog.push(
+        `• (<${issue.url}|${issue.identifier}>) ${issue.title} - _${assignee?.name}_`
+      );
+    } catch (error) {
+      core.warning(
+        `Failed to get assignee for issue ${issue.identifier}: ${error}`
+      );
+    }
+  }
+
+  return changelog.join("\n");
+}
+
+export async function sendToSlack(
+  client: WebClient,
+  channel: string,
+  text: string
+) {
+  await client.chat.postMessage({
+    channel,
+    text,
+    unfurl_links: false,
+    unfurl_media: false,
+    mrkdwn: true,
+  });
 }
